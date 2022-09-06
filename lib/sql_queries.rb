9a79b9ba48264e33f41a0df535b499457b98e347
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-09-06 03:15:48 -0700

    commit tree aed425169d8859973539f9826aace311f703a5d7

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,13 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+  );
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
